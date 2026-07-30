5c065603bd6f403947882dd0004cf73fad6d056d
Thu Ha <thuhaa0518@gmail.com>
2026-07-31 01:13:48 +0700

    Remove Prisma adapter to fix pgbouncer error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,9 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
 function createPrismaClient() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter } as ConstructorParameters<typeof PrismaClient>[0])
+  return new PrismaClient()
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
